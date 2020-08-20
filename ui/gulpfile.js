12b050233ac4b7adafbd0f9e6dcfa467b0132047
Randy Merrill <randy@blinkk.com>
2020-08-20 08:11:46 -0600

    Updating the gulpfile for newer changes.

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -1,20 +1,26 @@
-var extend = require('deep-extend');
-var fs = require('fs');
-var gulp = require('gulp');
-var gulpAutoprefixer = require('gulp-autoprefixer');
-var path = require('path');
-var readdirRecursive = require('fs-readdir-recursive');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var webpack = require('webpack');
-var webpackStream = require('webpack-stream');
-var WebpackBabiliPlugin = require("babili-webpack-plugin");
+const extend = require('deep-extend');
+const fs = require('fs');
+const {
+  dest,
+  series,
+  parallel,
+  src,
+  task,
+  watch
+} = require('gulp');
+const gulpAutoprefixer = require('gulp-autoprefixer');
+const path = require('path');
+const readdirRecursive = require('fs-readdir-recursive');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
 
-var config = {
+const config = {
   JS_SOURCE_DIR: './source/js/composite/',
   JS_SOURCES: [
     './partials/**/*.js',
-    './source/js/composite/**/*.js',
+    './source/js/**/*.js',
   ],
   JS_OUT_DIR: './dist/js/composite/',
   JS_OPTIONS: {
@@ -22,91 +28,71 @@ var config = {
       mangle: false
     }
   },
-  SASS_SOURCE_DIR: './source/sass/composite/**/*.sass',
+  SASS_SOURCE_DIR: './source/sass/composite/**/*.{sass,scss}',
   SASS_SOURCES: [
-    './partials/**/*.sass',
-    './source/sass/composite/**/*.sass',
+    './partials/**/*.{sass,scss}',
+    './source/sass/**/*.{sass,scss}',
   ],
   SASS_OUT_DIR: './dist/css/composite/'
 };
 
-var jsFiles = readdirRecursive(config.JS_SOURCE_DIR);
-var entry = {};
-jsFiles.forEach(function (value) {
+const jsFiles = readdirRecursive(config.JS_SOURCE_DIR);
+const entry = {};
+jsFiles.forEach(function(value) {
   if (value.endsWith('.js')) {
-    var key = value.substring(0, value.length - 3);
+    const key = value.substring(0, value.length - 3);
     entry[key] = config.JS_SOURCE_DIR + value;
   }
 });
 
-var webpackConfig = {
+const webpackConfig = {
   entry: entry,
-  module: {
-    loaders: [
-      { test: /\.json$/, loader: "json-loader" },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-    ],
-  },
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, config.JS_OUT_DIR),
     filename: '[name].min.js'
   }
 };
-var webpackProdConfig = extend({
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
-    new WebpackBabiliPlugin({}, {
-      comments: false
-    })
-  ]
-}, webpackConfig);
 
-gulp.task('compile-js', function() {
-  return gulp.src(config.JS_SOURCES)
-      .pipe(webpackStream(
-        webpackConfig, webpack
-      ))
-      .pipe(gulp.dest(config.JS_OUT_DIR));
+const webpackProdConfig = extend({}, webpackConfig, {
+  mode: 'production',
 });
 
-gulp.task('watch-js', () => {
-  webpackConfig.watch = true;
+const webpackWatchConfig = extend({}, webpackConfig, {
+  watch: true,
+});
 
-  gulp.src(config.JS_SOURCES)
+task('compile-js', function() {
+  return src(config.JS_SOURCES)
     .pipe(webpackStream(
-      webpackConfig, webpack
+      webpackProdConfig, webpack
     ))
-    .pipe(gulp.dest(config.JS_OUT_DIR));
+    .pipe(dest(config.JS_OUT_DIR));
 });
 
-gulp.task('compile-sass', function() {
-  gulp.src(config.SASS_SOURCE_DIR)
-  .pipe(sass({
-    outputStyle: 'compressed'
-  })).on('error', sass.logError)
-  .pipe(rename(function(path) {
-    path.basename += '.min';
-  }))
-  .pipe(gulpAutoprefixer({
-    browsers: [
-      'last 1 version',
-      'last 2 iOS versions'
-    ],
-  }))
-  .pipe(gulp.dest(config.SASS_OUT_DIR));
+task('compile-sass', function(cb) {
+  return src(config.SASS_SOURCE_DIR)
+    .pipe(sass({
+      outputStyle: 'compressed'
+    })).on('error', sass.logError)
+    .pipe(rename(function(path) {
+      path.basename += '.min';
+    }))
+    .pipe(gulpAutoprefixer())
+    .pipe(dest(config.SASS_OUT_DIR));
 });
 
-gulp.task('watch-sass', function() {
-  gulp.watch(config.SASS_SOURCES, ['compile-sass']);
+task('watch-sass', function() {
+  watch(config.SASS_SOURCES, series('compile-sass'));
 });
 
-gulp.task('build', ['compile-js', 'compile-sass']);
-gulp.task('grow-build', ['compile-js', 'compile-sass']);
-gulp.task('default', ['compile-js', 'compile-sass', 'watch-js', 'watch-sass']);
+task('watch-js', function() {
+  src(config.JS_SOURCES)
+  .pipe(webpackStream(webpackWatchConfig, webpack))
+  .pipe(dest(config.JS_OUT_DIR));
+});
+
+task('grow-build', parallel('compile-js', 'compile-sass'))
+
+exports.build = parallel('compile-js', 'compile-sass')
+exports.default = series('compile-sass', parallel('watch-js', 'watch-sass'))
